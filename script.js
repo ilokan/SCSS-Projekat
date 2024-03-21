a3fad12b005071fdc18d0734ad96088612443a3b
Nikola Miric <nikolamiric66@gmail.com>
2024-03-21 22:53:11 +0100

    js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const hamburger = document.querySelector(".hamburger");
 const navMeni = document.querySelector(".nav-meni");
-
+var color = 'red';
 hamburger.addEventListener("click", () =>{
     hamburger.classList.toggle("active");
     navMeni.classList.toggle("active");
 })
+function check(x) {
+    x.style.backgroundColor='color';
+}
